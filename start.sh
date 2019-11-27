8b58db02b0dd9aa6a87defa0c6814bf6fc5a9e62
Raphael Cabral <brcmesquita@gmail.com>
2019-11-27 09:26:42 -0300

    Add install NVM for NodeJS

--- start.sh
+++ start.sh
@@ -37,3 +37,11 @@
   sudo dpkg -i gitkraken-amd64.deb
   sudo apt install -fy
 
+echo "\n\e[01;36m ...\e[00m\n"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
