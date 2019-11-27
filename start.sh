b22d8a46060b1611a276e9c33ae27ca883edbb7d
Raphael Cabral <brcmesquita@gmail.com>
2019-11-27 09:16:02 -0300

    Add updates to distro

--- start.sh
+++ start.sh
@@ -5,4 +5,13 @@
 ## Created by Raphael Cabral
 ## Nov/26/2019
 
+  cd /tmp
+
+  echo "\n\e[01;36mAtualizando lista de repositórios...\e[00m\n"
+  sudo apt update
+  echo "\n\e[01;36mInicializando a atualização dos pacotes...\e[00m\n"
+  sudo apt upgrade -y
+  echo "\n\e[01;36mRemovendo pacotes que não são mais necessários...\e[00m\n"
+  sudo apt autoremove -y
+
   
